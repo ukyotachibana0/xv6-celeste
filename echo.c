012512ccefcf8d4d2cbc0ad30094e54c8fa85d89
ukyotachibana0 <3298825551@qq.com>
2020-11-29 08:55:36 +0800

    覆盖echo用户程序 实现功能：esc键按下和图片显示

--- echo.c
+++ echo.c
@@ -2,12 +2,37 @@
 #include "stat.h"
 #include "user.h"
 
+// int
+// main(int argc, char *argv[])
+// {
+//   int i;
+
+//   for(i = 1; i < argc; i++)
+//     printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+//   exit();
+// }
+
 int
 main(int argc, char *argv[])
 {
-  int i;
+  int keycode = 0x01;  // escape key pressed
 
-  for(i = 1; i < argc; i++)
-    printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  const int SCREEN_WIDTH = 320;
+  const int SCREEN_HEIGHT = 200;
+  const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
+  char* img = new char[SCREEN_SIZE];
+  memset(img, 0, SCREEN_SIZE);
+  for (int i = 0; i < SCREEN_HEIGHT; i++) {
+    for (int j = 0; j < SCREEN_WIDTH; j++) {
+      img[i * SCREEN_WIDTH * 3 + j * 3] = i;
+      img[i * SCREEN_WIDTH * 3 + j * 3 + 1] = i;
+      img[i * SCREEN_WIDTH * 3 + j * 3 + 2] = i;
+    }
+  }
+  
+  kbd(keycode);
+
+  imgdraw(img);
+
   exit();
 }
