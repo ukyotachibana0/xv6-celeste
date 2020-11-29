fe17370e288849cb17a9b137513eef198b1deda5
Shiqing <shiqing-thu18@yandex.com>
2020-11-29 23:07:38 +0800

    显示 CELESTE Classic 截图

--- echo.c
+++ echo.c
@@ -12,6 +12,8 @@
 //   exit();
 // }
 
+#include "img/CelesteClassic3_processed.h"
+
 int
 main(int argc, char *argv[])
 {
@@ -22,6 +24,7 @@ main(int argc, char *argv[])
   const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
   unsigned char* img = (unsigned char*)malloc(sizeof(char) * SCREEN_SIZE);
   memset(img, 0, SCREEN_SIZE);
+/*
   int i, j;
   for (i = 0; i < SCREEN_HEIGHT; i++) {
     for (j = 0; j < SCREEN_WIDTH; j++) {
@@ -38,6 +41,17 @@ main(int argc, char *argv[])
       
     }
   }
+*/
+
+  int x, y;
+  for (y = 0; y < 200; y++)
+    for (x = 0; x < 200; x++) {
+      int y1 = y;
+      int x1 = x + (SCREEN_WIDTH - 200) / 2;
+      img[(y1 * SCREEN_WIDTH + x1) * 3 + 0] = palette[image[y * 200 + x]][0];
+      img[(y1 * SCREEN_WIDTH + x1) * 3 + 1] = palette[image[y * 200 + x]][1];
+      img[(y1 * SCREEN_WIDTH + x1) * 3 + 2] = palette[image[y * 200 + x]][2];
+    }
   
   //kbd(keycode);
 
