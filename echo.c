3704befb2030909eb07ca628014abe2862bca7c6
Shiqing <shiqing-thu18@yandex.com>
2021-01-02 22:49:12 +0800

    在测试程序中加入头发闪烁

--- echo.c
+++ echo.c
@@ -24,38 +24,28 @@ main(int argc, char *argv[])
   const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
   unsigned char* img = (unsigned char*)malloc(sizeof(char) * SCREEN_SIZE);
   memset(img, 0, SCREEN_SIZE);
-/*
-  int i, j;
-  for (i = 0; i < SCREEN_HEIGHT; i++) {
-    for (j = 0; j < SCREEN_WIDTH; j++) {
-      if(i == 0 || i == 199 || j == 0 || j == 319){
-        img[i * SCREEN_WIDTH * 3 + j * 3] = 255;
-        img[i * SCREEN_WIDTH * 3 + j * 3 + 1] = 100;
-        img[i * SCREEN_WIDTH * 3 + j * 3 + 2] = 100;
-        continue;
-      } 
-      img[i * SCREEN_WIDTH * 3 + j * 3] = 100;
-      unsigned char _i = i/40;
-      img[i * SCREEN_WIDTH * 3 + j * 3 + 1] = 100;
-      img[i * SCREEN_WIDTH * 3 + j * 3 + 2] = _i * 40;
-      
-    }
-  }
-*/
 
-  int x, y;
-  for (y = 0; y < 200; y++)
-    for (x = 0; x < 200; x++) {
-      int y1 = y;
-      int x1 = x + (SCREEN_WIDTH - 200) / 2;
-      img[(y1 * SCREEN_WIDTH + x1) * 3 + 0] = palette[image[y * 200 + x]][0];
-      img[(y1 * SCREEN_WIDTH + x1) * 3 + 1] = palette[image[y * 200 + x]][1];
-      img[(y1 * SCREEN_WIDTH + x1) * 3 + 2] = palette[image[y * 200 + x]][2];
-    }
-  
   //kbd(keycode);
 
-  imgdraw(img);
+  int T = 0;
+  while (1) {
+    int x, y;
+    for (y = 0; y < 200; y++)
+      for (x = 0; x < 200; x++) {
+        int y1 = y;
+        int x1 = x + (SCREEN_WIDTH - 200) / 2;
+        int index = image[y * 200 + x];
+        if (index == 7) index = T % 14;
+        img[(y1 * SCREEN_WIDTH + x1) * 3 + 0] = palette[index][0];
+        img[(y1 * SCREEN_WIDTH + x1) * 3 + 1] = palette[index][1];
+        img[(y1 * SCREEN_WIDTH + x1) * 3 + 2] = palette[index][2];
+      }
+    imgdraw(img);
+    T++;
+    if (T % 30 == 0) printf(1, "frame: %d\n", T);
+    // TODO: Replace with vertical retrace
+    sleep(10);
+  }
   free(img);
 
   exit();
