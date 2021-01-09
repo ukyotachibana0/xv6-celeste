1781a546a9cd864b368df809e6905e155f69337b
Shiqing <shiqing-thu18@yandex.com>
2021-01-09 19:19:55 +0800

    更新 echo 测试程序，加入 kbd

--- echo.c
+++ echo.c
@@ -12,54 +12,47 @@
 //   exit();
 // }
 
-// #include "img/CelesteClassic3_processed.h"
+#include "img/CelesteClassic3_processed.h"
 
-// int
-// main(int argc, char *argv[])
-// {
-//   //int keycode = 0x01;  // escape key pressed
+int
+main(int argc, char *argv[])
+{
+  //int keycode = 0x01;  // escape key pressed
 
-//   const int SCREEN_WIDTH = 320;
-//   const int SCREEN_HEIGHT = 200;
-//   const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
-//   unsigned char* img = (unsigned char*)malloc(sizeof(char) * SCREEN_SIZE);
-//   memset(img, 0, SCREEN_SIZE);
+  const int SCREEN_WIDTH = 320;
+  const int SCREEN_HEIGHT = 200;
+  const int SCREEN_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
+  unsigned char* img = (unsigned char*)malloc(sizeof(char) * SCREEN_SIZE);
+  memset(img, 0, SCREEN_SIZE);
 
-//   //kbd(keycode);
+  //kbd(keycode);
 
-//   int T = 0;
-//   int i = 0;
-//   while (1) {
-//     T++;
-//     // if (T % 30 == 0) printf(1, "frame: %d\n", T);
-//     // TODO: Replace with vertical retrace
-//     printf(1, "%d\n", vretrace());
-//     if(vretrace()){
-//         i++;
-//     }
-//     if(i >= 350){
-//       // printf(1, "5s: %d\n", i);
-//       int x, y;
-//       for (y = 0; y < 200; y++)
-//         for (x = 0; x < 200; x++) {
-//           int y1 = y;
-//           int x1 = x + (SCREEN_WIDTH - 200) / 2;
-//           int index = image[y * 200 + x];
-//           if (index == 7) index = T % 14;
-//           img[(y1 * SCREEN_WIDTH + x1) * 3 + 0] = palette[index][0];
-//           img[(y1 * SCREEN_WIDTH + x1) * 3 + 1] = palette[index][1];
-//           img[(y1 * SCREEN_WIDTH + x1) * 3 + 2] = palette[index][2];
-//         }
-//       imgdraw(img);
-//       i = 0;
-//     }
-//     // sleep(10);
-//   }
-//   free(img);
-//   exit();
-// }
+  int T = 0;
+  while (1) {
+    T++;
+    // if (T % 30 == 0) printf(1, "frame: %d\n", T);
+    int key = kbd(1);
+    int x, y;
+    for (y = 0; y < 200; y++)
+      for (x = 0; x < 200; x++) {
+        int y1 = y;
+        int x1 = x + (SCREEN_WIDTH - 200) / 2;
+        int index = image[y * 200 + x];
+        if (index == 7) index = T % 14;
+        if (key && index == 3) index = (T + 7) % 14;
+        img[(y1 * SCREEN_WIDTH + x1) * 3 + 0] = palette[index][0];
+        img[(y1 * SCREEN_WIDTH + x1) * 3 + 1] = palette[index][1];
+        img[(y1 * SCREEN_WIDTH + x1) * 3 + 2] = palette[index][2];
+      }
+    imgdraw(img);
+    sleep(10);
+  }
+  free(img);
+  exit();
+}
 
 
+/*
 int
 main(int argc, char *argv[])
 {
@@ -70,3 +63,4 @@ main(int argc, char *argv[])
   }
   exit();
 }
+*/
